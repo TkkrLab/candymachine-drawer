9e633ce6db1721b78bedac3cf7212700f9b13088
Renze Nicolai <renze@rnplus.nl>
2018-07-17 16:25:11 +0200

    update :-)

--- candymachine-drawer.ino
+++ candymachine-drawer.ino
@@ -25,7 +25,6 @@
  * 
  */
 
-
 #include <Adafruit_NeoPixel.h>
 
 #define NEOPIXELS_PIN 8
@@ -33,20 +32,35 @@
 
 #define MOTORS_AMOUNT 3
 //#define MOTORS_AMOUNT 6
+#define UNIT_ID 6
 
-#define MOTOR1_PIN 2
-#define MOTOR2_PIN 3
-#define MOTOR3_PIN 4
-#define MOTOR4_PIN 5
-#define MOTOR5_PIN 6
-#define MOTOR6_PIN 7
-
-#define SWITCH1_PIN A0
-#define SWITCH2_PIN A1
-#define SWITCH3_PIN A2
-#define SWITCH4_PIN A3
-#define SWITCH5_PIN A4
-#define SWITCH6_PIN A5
+#if MOTORS_AMOUNT == 3
+  #define MOTOR1_PIN 4
+  #define MOTOR2_PIN 3
+  #define MOTOR3_PIN 2
+  #define MOTOR4_PIN 7
+  #define MOTOR5_PIN 6
+  #define MOTOR6_PIN 5
+  #define SWITCH1_PIN A2
+  #define SWITCH2_PIN A1
+  #define SWITCH3_PIN A0
+  #define SWITCH4_PIN A5
+  #define SWITCH5_PIN A4
+  #define SWITCH6_PIN A3
+#else
+  #define MOTOR1_PIN 7
+  #define MOTOR2_PIN 6
+  #define MOTOR3_PIN 5
+  #define MOTOR4_PIN 4
+  #define MOTOR5_PIN 3
+  #define MOTOR6_PIN 2
+  #define SWITCH1_PIN A5
+  #define SWITCH2_PIN A4
+  #define SWITCH3_PIN A3
+  #define SWITCH4_PIN A2
+  #define SWITCH5_PIN A1
+  #define SWITCH6_PIN A0
+#endif
 
 #define STATE_IDLE 0
 #define STATE_STARTING 1
@@ -55,25 +69,52 @@
 #define STATE_WAIT2 4
 #define STATE_CORRECTION 5
 
+#define TIMEOUT 15000
+
 const uint8_t motors[] = {MOTOR1_PIN, MOTOR2_PIN, MOTOR3_PIN, MOTOR4_PIN, MOTOR5_PIN, MOTOR6_PIN};
 const uint8_t switches[] = {SWITCH1_PIN, SWITCH2_PIN, SWITCH3_PIN, SWITCH4_PIN, SWITCH5_PIN, SWITCH6_PIN};
 
 unsigned long previousMillis = 0;
 const unsigned long interval = 100; //Interval in milliseconds
-uint8_t color = 0;
+
 uint8_t channel = 0;
 
 uint8_t correctioncounter[MOTORS_AMOUNT] = {0};
 
+uint8_t selectedLed = 0;
 
 uint8_t state[MOTORS_AMOUNT] = { 0 };
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXELS_AMOUNT, NEOPIXELS_PIN, NEO_GRBW + NEO_KHZ800);
 
-void moveChannel(uint8_t channel, bool mode = false) {
+unsigned long startTime;
+
+bool moveChannel(uint8_t channel, bool mode = false) {
   digitalWrite(motors[channel], HIGH);
-  while(digitalRead(switches[channel]) == mode);
+  startTime = millis();
+  while(digitalRead(switches[channel]) == mode) {
+    unsigned long currentTime = millis();
+    if (currentTime - startTime >= TIMEOUT) {
+      digitalWrite(motors[channel], LOW);
+      return false;
+    }
+  }
   digitalWrite(motors[channel], LOW);
+  return true;
+}
+
+void halt() {
+  Serial.println("#Halting...");
+  for (uint8_t i = 0; i<MOTORS_AMOUNT; i++) {
+    digitalWrite(motors[i], LOW);
+  }
+  Serial.println("state=halted");
+  Serial.println("#!!! HALTED !!!");
+  for (uint8_t i = 0; i<NEOPIXELS_AMOUNT; i++) {
+    strip.setPixelColor(i, (uint32_t) 0x00FF0000);
+  }
+  strip.show();
+  while(1);
 }
 
 void setup() {
@@ -83,171 +124,148 @@ void setup() {
   }
   
   Serial.begin(115200);
+  Serial.println("id="+String(UNIT_ID));
+  Serial.println("type=drawer");
+  Serial.println("state=booting");
+  Serial.println("boot=leds");
+
+  Serial.println("#Configuring LEDs...");
+  strip.begin();
   strip.setBrightness(255);
   for (uint8_t i = 0; i<NEOPIXELS_AMOUNT; i++) {
-    strip.setPixelColor(i, (uint32_t) 0x00FF0000);
+    strip.setPixelColor(i, (uint32_t) 0x00000000);
   }
-  
-  strip.begin();
-  
+  strip.setPixelColor((NEOPIXELS_AMOUNT-1)-(UNIT_ID-1), (uint32_t) 0x00006666);
   strip.show();
 
+  Serial.println("#Aligning motors...");
   for (uint8_t i = 0; i<MOTORS_AMOUNT; i++) {
-    moveChannel(i, true);
+    Serial.println("boot=aligning,"+String(i));
+    if (!moveChannel(i, true)) {
+      Serial.println("boot=error");
+      Serial.println("state=error");
+      Serial.println("error=motor,"+String(i)+",aligning");
+      Serial.println("#HARDWARE ERROR: CAN NOT ALIGN CHANNEL "+String(i));
+      halt();
+    }
   }
+  Serial.println("boot=ok");
+  Serial.println("state=ready");
 }
 
+
+bool prevBusy = false;
 void loop() {
+ while (Serial.available()>0) {
+      char c = Serial.read();
+      switch(c) {
+        case 'd': {
+          uint8_t motor = Serial.parseInt();
+          if (motor>=MOTORS_AMOUNT) {
+            Serial.println("error=motor,"+String(motor)+",invalid");
+            Serial.println("#Invalid motor");
+          } else {
+             if (state[motor]==STATE_IDLE) state[motor] = STATE_STARTING;
+          }
+          break;
+        }
+
+        case 'n': {
+          uint8_t motor = Serial.parseInt();
+          if (motor>=MOTORS_AMOUNT) {
+            Serial.println("error=motor,"+String(motor)+",invalid");
+            Serial.println("#Invalid motor");
+          } else {
+             if (state[motor]==STATE_IDLE) state[motor] = STATE_CORRECTION;
+          }
+          break;
+        }
+        
+        case 'i':
+          Serial.println("id="+String(UNIT_ID));
+          break;
+
+        case 'm':
+          Serial.println("motors="+String(MOTORS_AMOUNT));
+          break;
+
+        case 't':
+          Serial.println("type=drawer");
+          break;
+
+        case 's':
+          //Serial.print("?");
+          selectedLed = Serial.parseInt();
+          if (selectedLed>=NEOPIXELS_AMOUNT) selectedLed = NEOPIXELS_AMOUNT-1;
+          //Serial.println(selectedLed);
+          break;
+
+        case 'c': {
+          //Serial.print("?");
+          uint32_t color = Serial.parseInt();
+          color = (color<<8)+Serial.parseInt();
+          color = (color<<8)+Serial.parseInt();
+          color = (color<<8)+Serial.parseInt();
+          //Serial.println(color, HEX);
+          strip.setPixelColor((NEOPIXELS_AMOUNT-1)-selectedLed, color);
+          break;
+        }
+        case 'u':
+          strip.show();
+          //Serial.println("!");
+          break;
+      }
+    }
+  
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
-    for (uint8_t i = 0; i<NEOPIXELS_AMOUNT; i++) {
-
-      uint32_t statuscolor = 0;
-      
-      int8_t unit = 0;
-      #if (MOTORS_AMOUNT>5)
-        unit = -1;
-        if (i==0) unit = 0;
-        if (i==2) unit = 1;
-        if (i==3) unit = 2;
-        if (i==5) unit = 3;
-        if (i==6) unit = 4;
-        if (i==8) unit = 5;
-      #else
-        if (i>2) unit = 1;
-        if (i>5) unit = 2;
-      #endif
-
-      /*uint8_t led_in_unit = i - (unit*3);
-      int8_t progress = -1;
-      if (state[unit]==STATE_STARTING) progress = 1;
-      if (state[unit]==STATE_WAIT1)    progress = 1;
-      if (state[unit]==STATE_VENDING)  progress = 2;
-      if (state[unit]==STATE_WAIT2)    progress = 3;
-
-      if (progress>(2-led_in_unit)) statuscolor = 0x0000FF00;*/
-      if (unit>=0) {
-        if (state[unit]==STATE_STARTING) statuscolor = 0x00FF0000; //Red
-        if (state[unit]==STATE_WAIT1)    statuscolor = 0x00FF0000; //Red
-        if (state[unit]==STATE_VENDING)  statuscolor = 0x00FF0000; //Red
-        if (state[unit]==STATE_WAIT2)    statuscolor = 0x0000FF00; //Green
-      }
-      strip.setPixelColor(i, (uint32_t) statuscolor);
-    }
-    color++;
-    if (color>32) {
-      color = 0;
-      channel++;
-      if (channel>5) channel = 0;
-    }
 
     bool busy = false;
     for (uint8_t i = 0; i<MOTORS_AMOUNT; i++) {
-      if (state[i]!=STATE_IDLE) busy = true;
-    }
-
-    if (!busy) {
-      for (uint8_t i = 0; i<NEOPIXELS_AMOUNT; i++) {
-        strip.setPixelColor(i, ((uint32_t) color << (8*channel)));
-        //strip.setPixelColor(i, (uint32_t) 0xFFFFFFFF);
+      if (state[i]!=STATE_IDLE) {
+        busy = true;
+        break;
       }
-      //Serial.print(color);
-      //Serial.print("<< 8*");
-      //Serial.print(channel);
-      //Serial.print(" = ");
-      //Serial.println(((uint32_t)color << (8*channel)), HEX);
     }
-    
-    strip.show();
 
-    Serial.print(digitalRead(SWITCH1_PIN));
-    Serial.print(", ");
-    Serial.print(digitalRead(SWITCH2_PIN));
-    Serial.print(", ");
-    Serial.print(digitalRead(SWITCH3_PIN));
-    Serial.print(", ");
-    Serial.print(digitalRead(SWITCH4_PIN));
-    Serial.print(", ");
-    Serial.print(digitalRead(SWITCH5_PIN));
-    Serial.print(", ");
-    Serial.print(digitalRead(SWITCH6_PIN));
-    Serial.print(", ");
-    for (uint8_t i = 0; i<MOTORS_AMOUNT; i++) {
-      Serial.print(state[i]);
-      Serial.print(", ");
-    }
-    Serial.print(channel);
-    Serial.print(", ");
-    Serial.println(color);
-
-    while (Serial.available()>0) {
-      char c = Serial.read();
-      switch(c) {
-        case '1':
-          if (state[0]==STATE_IDLE) state[0] = STATE_STARTING;
-          break;
-        case '2':
-          if (state[1]==STATE_IDLE) state[1] = STATE_STARTING;
-          break;
-        case '3':
-          if (state[2]==STATE_IDLE) state[2] = STATE_STARTING;
-          break;
-        #if (MOTORS_AMOUNT>5)
-          case '4':
-            if (state[3]==STATE_IDLE) state[3] = STATE_STARTING;
-            break;
-          case '5':
-            if (state[4]==STATE_IDLE) state[4] = STATE_STARTING;
-            break;
-          case '6':
-            if (state[5]==STATE_IDLE) state[5] = STATE_STARTING;
-            break;
-        #endif
-        
-        case 'q':
-          if (state[0]==STATE_IDLE) state[0] = STATE_CORRECTION;
-          break;
-        case 'w':
-          if (state[1]==STATE_IDLE) state[1] = STATE_CORRECTION;
-          break;
-        case 'e':
-          if (state[2]==STATE_IDLE) state[2] = STATE_CORRECTION;
-          break;
-
-        #if (MOTORS_AMOUNT>5)
-        case 'r':
-          if (state[3]==STATE_IDLE) state[3] = STATE_CORRECTION;
-          break;
-        case 't':
-          if (state[4]==STATE_IDLE) state[4] = STATE_CORRECTION;
-          break;
-        case 'y':
-          if (state[5]==STATE_IDLE) state[5] = STATE_CORRECTION;
-          break;
-        #endif
+    if (prevBusy!=busy) {
+      prevBusy = busy;
+      if (busy) {
+        Serial.println("state=busy");
+      } else {
+        Serial.println("state=ready");
       }
     }
 
     for (uint8_t i = 0; i<MOTORS_AMOUNT; i++) {
       switch(state[i]) {
         case STATE_STARTING:
+          startTime = millis();
           state[i] = STATE_WAIT1;
           digitalWrite(motors[i], HIGH); //Start motor
-          Serial.println("Preparing to vend product "+String(i)+"...");
+          Serial.println("#Preparing to vend product "+String(i)+"...");
           break;
         case STATE_WAIT1:
           //Wait until button gets pressed
           if (!digitalRead(switches[i])) {
             state[i] = STATE_VENDING;
-            Serial.println("Vending product "+String(i)+"...");
+            Serial.println("#Vending product "+String(i)+"...");
+          } else if (millis() - startTime >= TIMEOUT) {
+            Serial.println("state=error");
+            Serial.println("error=motor,"+String(i)+",preparing");
+            halt();
           }
           break;
         case STATE_VENDING:
           //Wait until button gets released
           if (digitalRead(switches[i])) {
             state[i] = STATE_WAIT2;
-            Serial.println("Finishing vending process for product "+String(i)+"...");
+            Serial.println("#Finishing vending process for product "+String(i)+"...");
+          } else if (millis() - startTime >= TIMEOUT) {
+            Serial.println("state=error");
+            Serial.println("error=motor,"+String(i)+",vending");
+            halt();
           }
           break;
         case STATE_WAIT2:
@@ -255,7 +273,11 @@ void loop() {
           if (!digitalRead(switches[i])) {
             state[i] = STATE_IDLE;
             digitalWrite(motors[i], LOW);
-            Serial.println("Vending product "+String(i)+" completed.");
+            Serial.println("#Vending product "+String(i)+" completed.");
+          }else if (millis() - startTime >= TIMEOUT) {
+            Serial.println("state=error");
+            Serial.println("error=motor,"+String(i)+",finising");
+            halt();
           }
           break;
         case STATE_CORRECTION:
@@ -263,9 +285,11 @@ void loop() {
             state[i] = STATE_IDLE;
             correctioncounter[i] = 0;
             digitalWrite(motors[i], LOW);
-            Serial.println("Corrected "+String(i)+".");
+            Serial.println("#Corrected "+String(i)+".");
           } else {
-            Serial.println("Correcting "+String(i)+": "+String(correctioncounter[i])+"....");
+            Serial.println("state=correcting");
+            Serial.println("corr="+String(i)+","+String(correctioncounter[i]));
+            Serial.println("#Correcting "+String(i)+": "+String(correctioncounter[i])+"....");
             digitalWrite(motors[i], HIGH);
           }
           correctioncounter[i]++;
